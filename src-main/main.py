95ef43d637ab71b834239695a98a567f0b743a9b
Fesaa <77553571+Fesaa@users.noreply.github.com>
2022-07-10 19:00:38 +0200

    Subclassing + Logging

--- src-main/main.py
+++ src-main/main.py
@@ -1,61 +1,70 @@
-from typing import Union
+import os
+import sys
+import traceback
+import discord
+import logging
+import logging.handlers
+
 from discord.ext import commands
-from os import listdir, chdir, path
-from asyncio import run as asynciorun
-from discord.errors import LoginFailure
-from discord.app_commands import errors as app_errors
-from discord import Intents, Object, app_commands, Interaction
 
-chdir(path.dirname(path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-from cogs.config_handler import host, Database, user, password, token, get_prefix, bot_id
-from imports.database import DataBase
-from imports.functions import time
+from cogs.ConfigHandler import host, Database, user, password, token, get_prefix, bot_id
+from imports.db import DataBase
 
-bot = commands.Bot(command_prefix=get_prefix, intents=Intents().all(), case_insensitive=True, help_command=None, application_id=bot_id)
-bot.db = DataBase(host=host, database=Database, password=password, user=user)
-tree = bot.tree
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
 
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-@bot.event
-async def on_ready():
-    print(time() + f" Code running on account with name: {bot.user.name} and id {bot.user.id}")
+class Botty(commands.Bot):
 
-async def load_extensions():
-    for filename in listdir("./cogs"):
-        if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")
+    def __init__(self) -> None:
 
+        allowed_mentions = discord.AllowedMentions(roles=True, users=True, everyone=False)
+        intents= discord.Intents(
+            guilds=True,
+            members=True,
+            bans=True,
+            emojis=True,
+            voice_states=False,
+            messages=True,
+            reactions=True,
+            message_content=True,
+        )
+        case_insensitive=True
 
-async def main():
-    async with bot:
-        await load_extensions()
-        try:
-            await bot.start(token)
-        except LoginFailure:
-            print("Can't log in, please check your token in config.json!")
-        
+        super().__init__(command_prefix=get_prefix, allowed_mentions=allowed_mentions, intents=intents, case_insensitive=case_insensitive,  application_id=bot_id)
 
-@bot.command()
-async def sync_slash(ctx: commands.Context):
-    await bot.tree.sync()
-    await ctx.send("Tree synced!")
+    async def setup_hook(self) -> None:
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                try:
+                    await self.load_extension(f'cogs.{filename[:-3]}')
+                except Exception as e:
+                    print(f'Failed to load extension {filename}.', file=sys.stderr)
+                    traceback.print_exc()
 
-@tree.error
-async def app_command_error(interaction: Interaction, error: app_commands.AppCommandError):
-    if isinstance(error, app_errors.MissingPermissions):
-        await interaction.response.send_message('You are missing the nessairy perms to use this command.', ephemeral=True)
-    elif isinstance(error, app_errors.CommandNotFound):
-        pass
-    else:
-        raise error
+    async def on_ready(self) -> None:
+        print(f'Ready: {self.user} (ID: {self.user.id})')
 
-@commands.Cog.listener()
-async def on_command_error(self, ctx, exc):
-    if isinstance(exc, commands.errors.CommandNotFound) or isinstance(exc, commands.errors.MissingRequiredArgument):
-        pass
-    else:
-        raise exc
+def main():
+    bot = Botty()
+    bot.db: DataBase = DataBase(host=host, database=Database, password=password, user=user)
+    try:
+        bot.run(token, log_handler=None)
+    except discord.LoginFailure:
+        print('Invalid token', file=sys.stderr)
 
-
-asynciorun(main())
+if __name__ == '__main__':
+    main()
