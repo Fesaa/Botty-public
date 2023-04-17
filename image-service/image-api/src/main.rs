8da0415cc18beee7cebebb456b16c78973911693
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-04-17 23:31:41 +0200

    Config file

--- image-service/image-api/src/main.rs
+++ image-service/image-api/src/main.rs
@@ -1,12 +1,23 @@
 use std::sync::Arc;
+use toml;
 
 use actix_web::{App,HttpServer, web::Data};
 use image_renderer::ImageRenderer;
+use serde::Deserialize;
 
 mod routes;
 
+#[derive(Deserialize)]
+struct Config {
+    address: String,
+    port: u16,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let config: Config = toml::from_str(&std::fs::read_to_string("config.toml")?).unwrap();
+
     HttpServer::new(|| {
         App::new()
             .app_data(Data::from(Arc::new(ImageRenderer::new())))
@@ -14,7 +25,7 @@ async fn main() -> std::io::Result<()> {
             .service(routes::get_image_renderer_cube_level_level)
             .service(routes::get_image_renderer_cube_level_level_distance)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((config.address, config.port))?
     .run()
     .await
 }
